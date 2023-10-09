83724f7c181f8d43e1c50a24a24fe1132fce4ee7
isSatake <stkayy@gmail.com>
2023-10-09 12:34:56 +0900

    prevent context menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
 import { Keyboard } from "./Keyboard";
 import { MainCanvas } from "./MainCanvas";
 import { PreviewCanvas } from "./PreviewCanvas";
 
-
 export default function Home() {
+  useEffect(() => {
+    window.addEventListener("contextmenu", (e) => e.preventDefault());
+  }, []);
   return (
     <>
       <MainCanvas />
