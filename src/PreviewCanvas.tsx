f3d04826d172f75a1c498d63621acf5c73f6a73b
isSatake <stkayy@gmail.com>
2024-02-29 10:28:00 +0900

    transform

--- src/PreviewCanvas.tsx
+++ src/PreviewCanvas.tsx
@@ -18,11 +18,11 @@ const htmlHeight = getPreviewHeight();
 // B4がcanvasのvertical centerにくるように
 const topOfStaff = htmlHeight / 2 - (bStaffHeight * getPreviewScale()) / 2;
 
-const mtxAtom = atom<DOMMatrix | undefined>(undefined);
+// x: 左端 y: 中心
+const mtx = new DOMMatrix([1, 0, 0, 1, 0, topOfStaff]).scale(getPreviewScale());
 
 export const PreviewCanvas = ({ preview }: { preview: PreviewState }) => {
   const ref = useRef<HTMLCanvasElement>(null);
-  const [mtx, setMtx] = useAtom(mtxAtom);
   useEffect(() => {
     const canvas = ref.current!;
     // TODO MainCanvasと同様に最大scaleを決める
@@ -35,12 +35,7 @@ export const PreviewCanvas = ({ preview }: { preview: PreviewState }) => {
     resetCanvas2({ ctx: canvas.getContext("2d")!, fillStyle: "white" });
   }, []);
   useEffect(() => {
-    // x: 左端 y: 中心
-    setMtx(new DOMMatrix([1, 0, 0, 1, 0, topOfStaff]).scale(getPreviewScale()));
-  }, []);
-  useEffect(() => {
     console.log("preview", "start");
-    if (!preview.staff || !preview.elements || !mtx) return;
     const styles = determinePaintElementStyle(
       preview.elements,
       UNIT,
@@ -54,7 +49,11 @@ export const PreviewCanvas = ({ preview }: { preview: PreviewState }) => {
       if (index !== undefined) {
         elIdxToX.set(index, cursor + width / 2);
       }
-      if (element.type !== "beam" && element.type !== "tie") {
+      if (
+        element.type !== "staff" &&
+        element.type !== "beam" &&
+        element.type !== "tie"
+      ) {
         cursor += width;
       }
     }
@@ -71,7 +70,11 @@ export const PreviewCanvas = ({ preview }: { preview: PreviewState }) => {
     for (const style of styles) {
       paintStyle(ctx, style);
       // paintBBox(ctx, style.bbox); // debug
-      if (style.element.type !== "beam" && style.element.type !== "tie") {
+      if (
+        style.element.type !== "staff" &&
+        style.element.type !== "beam" &&
+        style.element.type !== "tie"
+      ) {
         ctx.translate(style.width, 0);
       }
     }
