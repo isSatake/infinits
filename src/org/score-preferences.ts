a759f65ff0e701bab516978671137310a966c7bc
isSatake <stkayy@gmail.com>
2024-05-24 19:03:37 +0900

    wip

--- src/org/score-preferences.ts
+++ src/org/score-preferences.ts
@@ -3,14 +3,14 @@ export const kDefaultCaretWidth = 50;
 const initScale = 0.08;
 export const getInitScale = () => initScale;
 
-const previewScale = 0.1;
+const previewScale = 0.3;
 export const getPreviewScale = () => previewScale;
 
 const staffOrigin = { x: 250, y: 2000 };
 export const getStaffOrigin = () => staffOrigin;
 
-const previewWidth = 400;
+const previewWidth = 800;
 export const getPreviewWidth = () => previewWidth;
 
-const previewHeight = previewWidth * 1.5;
+const previewHeight = previewWidth;
 export const getPreviewHeight = () => previewHeight;
