ed84433a88fb302a3340a6ae8f71a97f578fae22
isSatake <stkayy@gmail.com>
2024-06-11 17:00:04 +0900

    remove init notes

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,4 @@
 import { MusicalElement } from "@/org/notation/types";
 import { UNIT } from "./org/font/bravura";
 
-export const kSampleElements: MusicalElement[] = [
-  { type: "note", duration: 4, pitches: [{ pitch: 1 }], tie: "start" },
-  { type: "note", duration: 4, pitches: [{ pitch: 1 }], tie: "stop" },
-];
-
 export const kDefaultStaffWidth = UNIT * 50;
