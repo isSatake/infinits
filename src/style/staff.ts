90c3db38adb0569e99889ebcaa516cc21bef3205
isSatake <stkayy@gmail.com>
2025-03-09 16:04:38 +0900

    wip

--- src/style/staff.ts
+++ src/style/staff.ts
@@ -3,31 +3,26 @@ import { PaintElementStyle, StaffConnectionStyle, StaffStyle } from "./types";
 import { Point } from "@/lib/geometry";
 
 export const buildConnectionStyle: (
-  from: StaffStyle,
-  fromPos: Point,
   fromStyle: PaintElementStyle<StaffStyle>,
-  to: StaffStyle
-) => PaintElementStyle<StaffConnectionStyle> = (
-  from,
-  fromPos,
-  fromStyle,
-  to
-) => {
-  const toPos = to.position;
+  to: Point
+) => PaintElementStyle<StaffConnectionStyle> = (fromStyle, to) => {
+  const fromStaff = fromStyle.element;
+  const fromLines = fromStaff.lines;
+  const fromPos = fromStaff.position;
   const toPoint: Point = {
-    x: toPos.x - fromPos.x - fromStyle.width,
-    y: toPos.y - fromPos.y,
+    x: to.x - fromPos.x - fromStyle.width,
+    y: to.y - fromPos.y,
   };
   const connectionStyle: PaintElementStyle<StaffConnectionStyle> = {
-    element: { type: "staffConnection", to: toPoint, lines: from.lines },
+    element: { type: "staffConnection", to: toPoint, lines: fromLines },
     width: 0, // 現状connectionの右に何か描画することはないので0にしておく
     bbox: {
       left: 0,
       top: Math.min(0, toPoint.y),
       right: toPoint.x,
       bottom: Math.max(
-        (from.lines.length - 1) * UNIT,
-        toPos.y - fromPos.y + (to.lines.length - 1) * UNIT
+        (fromLines.length - 1) * UNIT,
+        to.y - fromPos.y + (fromLines.length - 1) * UNIT
       ),
     },
   };
