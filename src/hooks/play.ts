85a0c2948b07c0d380ab339e64ea99d4e24353f0
isSatake <stkayy@gmail.com>
2025-03-14 13:16:33 +0900

    fix play

--- src/hooks/play.ts
+++ src/hooks/play.ts
@@ -14,38 +14,41 @@ export const usePlayTone = () => {
   const { rootObjId } = useAtomValue(focusAtom);
   const elementsMap = useAtomValue(elementsAtom);
   const connection = useAtomValue(connectionAtom);
-  // 0 - 1 - 2
-  //     \ - 3
-  // -> [[-1, { elements: [0, 1, 2]}], [1, { elements: [3]}]]
-  const fragments = new Map<
-    number,
-    { rootObjId: number; elements: (MusicalElement | FileStyle)[] }[]
-  >();
-  const processedIds: number[] = [];
-  const processId = (rootObjId: number) => {
-    const obj = rootObjs.get(rootObjId);
-    if (!obj) return;
-    const elements: (MusicalElement | FileStyle)[] = [];
-    if (obj?.type === "file") {
-      elements.push(obj);
-    }
-    if (obj?.type === "staff") {
-      elements.push(...(elementsMap.get(rootObjId) ?? []));
-    }
-    return { rootObjId, elements };
+
+  return () => {
+    // 0 - 1 - 2
+    //     \ - 3
+    // -> [[-1, { elements: [0, 1, 2]}], [1, { elements: [3]}]]
+    const fragments = new Map<
+      number,
+      { rootObjId: number; elements: (MusicalElement | FileStyle)[] }[]
+    >();
+    const processedIds: number[] = [];
+    const processId = (rootObjId: number) => {
+      const obj = rootObjs.get(rootObjId);
+      if (!obj) return;
+      const elements: (MusicalElement | FileStyle)[] = [];
+      if (obj?.type === "file") {
+        elements.push(obj);
+      }
+      if (obj?.type === "staff") {
+        elements.push(...(elementsMap.get(rootObjId) ?? []));
+      }
+      return { rootObjId, elements };
+    };
+    const searchFragments = (prevId: number, currentId: number) => {
+      if (processedIds.includes(currentId)) return;
+      processedIds.push(currentId);
+      const elements = processId(currentId);
+      if (!elements) return;
+      fragments.set(prevId, (fragments.get(prevId) ?? []).concat(elements));
+      const connectionIds = connection.get(currentId);
+      if (!connectionIds) return;
+      const [nextId, ...others] = connectionIds;
+      searchFragments(prevId, nextId);
+      others.map((_id) => searchFragments(currentId, _id));
+    };
+    searchFragments(-1, rootObjId);
+    tone.multiPlay(fragments);
   };
-  const searchFragments = (prevId: number, currentId: number) => {
-    if (processedIds.includes(currentId)) return;
-    processedIds.push(currentId);
-    const elements = processId(currentId);
-    if (!elements) return;
-    fragments.set(prevId, (fragments.get(prevId) ?? []).concat(elements));
-    const connectionIds = connection.get(currentId);
-    if (!connectionIds) return;
-    const [nextId, ...others] = connectionIds;
-    searchFragments(prevId, nextId);
-    others.map((_id) => searchFragments(currentId, _id));
-  };
-  searchFragments(-1, rootObjId);
-  return () => tone.multiPlay(fragments);
 };
